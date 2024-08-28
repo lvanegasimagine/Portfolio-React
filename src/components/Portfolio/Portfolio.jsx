49bcf3bc2c4524a4d49f190073fd5b9cd6a703ee
Luis Vanegas <lvanegas1429@gmail.com>
2024-08-28 11:05:53 -0600

    Update portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -3,8 +3,7 @@ import "./portfolio.css";
 import IMG1 from "../../assets/pacientes.png";
 import IMG2 from "../../assets/portfolio2.png";
 import IMG3 from "../../assets/portfolio3.png";
-import IMG4 from "../../assets/portfolio4.jpg";
-import IMG5 from "../../assets/portfolio5.jpg";
+import IMG4 from "../../assets/portfolio4.png";
 import IMG6 from "../../assets/portfolio6.jpg";
 import { FaGithub } from "react-icons/fa";
 
@@ -41,7 +40,7 @@ const Portfolio = () => {
           </div>
           <h3>Monedero React + Firebase</h3>
           <div className="portfolio__item-cta">
-            <a href="https://github.com/lvanegasimagine/My-Money---React.git" className="btn">
+            <a href="https://github.com/lvanegasimagine/My-Money---React.git" className="btn" target="_blank">
             <FaGithub />
             </a>
             <a
@@ -59,7 +58,7 @@ const Portfolio = () => {
           </div>
           <h3>Control de Gastos React + Localstorage</h3>
           <div className="portfolio__item-cta">
-            <a href="https://github.com/lvanegasimagine/control-gastos-react" className="btn">
+            <a href="https://github.com/lvanegasimagine/control-gastos-react" className="btn" target="_blank">
             <FaGithub />
             </a>
             <a
@@ -75,31 +74,13 @@ const Portfolio = () => {
           <div className="portfolio__item-image">
             <img src={IMG4} alt="Portfolio 4" />
           </div>
-          <h3>Clone Airbnb Nextjs + Mongo + Prisma</h3>
+          <h3>Saas Landing Page Nextjs + Tailwindcss + Framer Motion</h3>
           <div className="portfolio__item-cta">
-            <a href="https://github.com/lvanegasimagine/Nextjs13-Airbnb" className="btn">
+            <a href="https://github.com/lvanegasimagine/saas-landing-page" className="btn" target="_blank">
             <FaGithub />
             </a>
             <a
-              href="https://nextjs13-airbnb.vercel.app/"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG5} alt="Portfolio 5" />
-          </div>
-          <h3>SpaceX Mission Api</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com/lvanegasimagine/spacex-missions" className="btn">
-            <FaGithub />
-            </a>
-            <a
-              href="https://spacex-missions-nine.vercel.app/"
+              href="https://saas-landing-page-eta-two.vercel.app/"
               className="btn btn-primary"
               target="_blank"
             >
@@ -113,7 +94,7 @@ const Portfolio = () => {
           </div>
           <h3>Pokedex Nextjs</h3>
           <div className="portfolio__item-cta">
-            <a href="https://github.com/lvanegasimagine/pokemon-nextjs.git" className="btn">
+            <a href="https://github.com/lvanegasimagine/pokemon-nextjs.git" className="btn" target="_blank">
             <FaGithub />
             </a>
             <a
